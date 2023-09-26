aa41bfdc8aa7b09a4dcf7c5bd0fd75eea4b901d7
jihad hossain <jihadkhan934@gmail.com>
2023-09-26 07:26:55 -0700

    s

--- blank.js
+++ blank.js
@@ -3,11 +3,33 @@
 
 
 
+const arryObj= ['mango','mango', 'apple','apple','banana','pineapple','1','1']
 
+const arOb= [
+    {name: 'jihad',
+age: 22},
+    {name: 'rakib',
+age: 22},
+    {name: 'jihad',
+age: 22},
+    {name: 'rakib',
+age: 22},
+    {name: 'jihad',
+age: 22},
+]
 
+// function removeDuplicateObj(data){
+//     // console.log(data)
+//     return data.filter((item,index)=> data.indexOf(item) === index)
+// }
+const names = arOb.map(i=>i.name)
+function removeDuplicateObj2(data){
+    // console.log(data)
+    return data.filter(({ name, price }, index) => !names.includes(name, index + 1))
+}
 
-
-
+// console.log(removeDuplicateObj(arryObj))
+console.log(removeDuplicateObj2(arOb))
 
 
 
